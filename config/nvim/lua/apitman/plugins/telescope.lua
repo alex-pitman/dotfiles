7143122613da81f2c43bf7ec697470d0c05d7dbe
alexander.pitman <alexander.pitman@care.com>
2026-06-25 11:33:15 -0600

    fix: point telescope to master branch

--- config/nvim/lua/apitman/plugins/telescope.lua
+++ config/nvim/lua/apitman/plugins/telescope.lua
@@ -1,32 +1,33 @@
 return {
-  'nvim-telescope/telescope.nvim',
+  "nvim-telescope/telescope.nvim",
+  branch = "master",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-tree/nvim-web-devicons',
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require('telescope').setup {
+    require("telescope").setup({
       extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
         },
       },
-    }
+    })
 
     -- Enabble Telescope extensions
-    require('telescope').load_extension 'fzf'
-    require('telescope').load_extension 'ui-select'
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("ui-select")
 
     -- Setup keymaps
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]search [F]files' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]search by [G]rep' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]search [W]ord under cursor' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]search [H]help' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]search in [K]eymaps' })
-    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]search in open [B]uffers' })
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]search [F]files" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]search by [G]rep" })
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]search [W]ord under cursor" })
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]search [H]help" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]search in [K]eymaps" })
+    vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]search in open [B]uffers" })
   end,
 }
